c45650ecdec94c7ea9da884752ed3e10a9485c0b
mthsmelo <matheusaguiarmelo@gmail.com>
2023-03-03 15:45:47 -0300

    req 16, 17 and 18

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React from 'react';
-import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import './App.css'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/"component = {} />
+        <Route exact path="/meals"component = {}  /* Precisa ter componente <Footer/> no final do componente */ /> 
+        <Route exact path="/drinks" component = {}  /* Precisa ter componente <Footer/> no final do componente *//>
+        <Route exact path="/meals/:id-da-receita" component = {}/>
+        <Route exact path="/drinks/:id-da-receita"component = {} />
+        <Route exact path="/meals/:id-da-receita/in-progress"component = {} />
+        <Route exactpath="/drinks/:id-da-receita/in-progress" component = {} />
+        <Route exact path="/profile" component = {}  /* Precisa ter componente <Footer/> no final do componente *//>
+        <Route exact path="/done-recipes"component = {} />
+        <Route exact path="/favorite-recipes"component = {} />
+        <Route exact path="*"component = {} />
+      </Switch>
+    </BrowserRouter>
+
   );
 }
 
