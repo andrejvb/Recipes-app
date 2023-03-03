a44e2514bb260219f8e40f9e3f3658fc6d195408
lu1z <luiz.alberto.laus.rosa@gmail.com>
2023-03-03 16:31:27 -0300

    merge matheus

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import './App.css'; 
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/"component = {} />
-        <Route exact path="/meals"component = {}  /* Precisa ter componente <Footer/> no final do componente */ /> 
-        <Route exact path="/drinks" component = {}  /* Precisa ter componente <Footer/> no final do componente *//>
-        <Route exact path="/meals/:id-da-receita" component = {}/>
-        <Route exact path="/drinks/:id-da-receita"component = {} />
-        <Route exact path="/meals/:id-da-receita/in-progress"component = {} />
-        <Route exactpath="/drinks/:id-da-receita/in-progress" component = {} />
-        <Route exact path="/profile" component = {}  /* Precisa ter componente <Footer/> no final do componente *//>
-        <Route exact path="/done-recipes"component = {} />
-        <Route exact path="/favorite-recipes"component = {} />
-        <Route exact path="*"component = {} />
+        <Route exact path="/" component={ Footer } />
+        {/* Precisa ter componente <Footer/> no final do componente */}
+        <Route exact path="/meals" component={ Footer } />
+        {/* Precisa ter componente <Footer/> no final do componente */}
+        <Route exact path="/drinks" component={ Footer } />
+        <Route exact path="/meals/:id-da-receita" component={ Footer } />
+        <Route exact path="/drinks/:id-da-receita" component={ Footer } />
+        <Route exact path="/meals/:id-da-receita/in-progress" component={ Footer } />
+        <Route exactpath="/drinks/:id-da-receita/in-progress" component={ Footer } />
+        {/* Precisa ter componente <Footer/> no final do componente */}
+        <Route exact path="/profile" component={ Footer } />
+        <Route exact path="/done-recipes" component={ Footer } />
+        <Route exact path="/favorite-recipes" component={ Footer } />
+        <Route exact path="*" component={ Footer } />
       </Switch>
     </BrowserRouter>
 
