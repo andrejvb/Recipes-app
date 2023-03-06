7df0a8ee4b6f76897e4e7c135cd242ba9daa6475
Thalita <thalitacostasantos12@gmail.com>
2023-03-06 12:01:44 -0300

    Requisitos 5 e 6 finalizados

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { screen, render } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
-// import { renderWithRouter } from '../helpers/renderWithRouter';
+import renderWithRouter from './helpers/renderWithRouter';
 // import Login from '../components/Login';
 
 describe('Testes da página de Login', () => {
   it('', () => {
-    render(<App />);
+    renderWithRouter(<App />);
     const emailId = screen.getByTestId('email-input');
     expect(emailId).toBeInTheDocument();
     const senha = screen.getByTestId('password-input');
@@ -21,5 +21,18 @@ describe('Testes da página de Login', () => {
     userEvent.type(senha, '1234567');
     expect(botao).toBeEnabled();
     userEvent.click(botao);
+  });
+  it('Testa se é possível digitar nos campos de email e senha', () => {
+    renderWithRouter(<App />);
+
+    const email = screen.getByTestId('email-input');
+
+    const senha = screen.getByTestId('password-input');
+
+    userEvent.type(email, 'tha@gmail.com');
+    userEvent.type(senha, '12345678');
+
+    expect(email.value).toBe('tha@gmail.com');
+    expect(senha.value).toBe('12345678');
   });
 });
