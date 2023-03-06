bc31758f6073edb6941a530350c37d4b7add5cb0
Sadira <sadiraoliveira@gmail.com>
2023-03-06 14:35:25 -0300

    Requisito 20 feito 90%

--- src/ServicesRecipes/apiFood.js
+++ src/ServicesRecipes/apiFood.js
@@ -1,6 +1,13 @@
 export const apiRecipesFood = async () => {
   const fetchApiFood = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
   const apiFoodResponse = await fetchApiFood.json();
-  console.log(apiFoodResponse);
+  // console.log(apiFoodResponse);
   return apiFoodResponse;
-};
+}; // api com todas as chaves para renderizar as 12 receitas na tela
+
+export const apiRecipesSetFood = async () => {
+  const fetchApiFood = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list');
+  const apiFoodResponse = await fetchApiFood.json();
+  // console.log(apiFoodResponse);
+  return apiFoodResponse;
+}; // api com as categorias e renderizar os 5 botões na tela
