ce868c22b857d3da2379bcdc8a94dbe6b2eb7e79
Sadira <sadiraoliveira@gmail.com>
2023-03-06 22:18:38 -0300

    Requisito 21

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 function Card({ index, title, thumb }) {
+  console.log(thumb);
   return (
     <figure data-testid={ `${index}-recipe-card` }>
       <figcaption data-testid={ `${index}-card-name` }>{ title }</figcaption>
