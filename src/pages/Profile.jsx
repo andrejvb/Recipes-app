c1c8f36aeeaf8b5771fda3b76e58277fb418f52e
Sadira <sadiraoliveira@gmail.com>
2023-03-08 11:56:29 -0300

    Requisito 9, criado os 3 botões

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
+// import { Button } from 'bootstrap';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
@@ -8,6 +9,24 @@ function Profile() {
     <>
       <Header />
       <div>Profile</div>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+      >
+        Done Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+      >
+        Favorite Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+      >
+        Logout
+      </button>
       <Footer />
     </>
   );
