dc2207697d8db945d06b24279b50c864e7df8340
mthsmelo <matheusaguiarmelo@gmail.com>
2023-03-10 04:25:07 -0300

    requirement 41 ok! missing tests, and requirement 40

--- src/components/DrinkInProgressCard.jsx
+++ src/components/DrinkInProgressCard.jsx
@@ -1,24 +1,38 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import copy from 'clipboard-copy';
 import shareIcon from '../images/shareIcon.svg';
 import blackHeartIcon from '../images/blackHeartIcon.svg';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 import '../style/checkboxStyle.css';
 
-export default function DrinkInProgressCard({ fetchReturn/* , typeOfRecipe */ }) {
-  const [checkIgredient, setCheckIgredient] = useState({});
+export default function DrinkInProgressCard({ fetchReturn, typeOfRecipe }) {
+  const [checkIgredient, setCheckIgredient] = useState(() => {
+    const save = localStorage.getItem('inProgressRecipes');
+    const initialValue = JSON.parse(save);
+    return initialValue || {};
+  });
+  const [copied, setCopied] = useState(false);
+  const [favorited, setFavorited] = useState(false);
   const [isDisabled, setIsDisabled] = useState(true);
   const ingredients = Object.entries(fetchReturn)
     .filter((ing) => ing[0].includes('Ingredient'))
     .filter((ing) => ing[1] !== '' && ing[1] !== null);
   const measures = Object.entries(fetchReturn)
     .filter((ing) => ing[0].includes('Measure')).slice(0, ingredients.length);
+  const favoriteLS = localStorage.getItem('favoriteRecipes')
+    ? JSON.parse(localStorage.getItem('favoriteRecipes'))
+    : localStorage.setItem('favoriteRecipes', JSON.stringify([]));
 
   useEffect(() => {
     const obj = {};
-    for (let i = 0; i < ingredients.length; i += 1) {
-      obj[i] = false;
-    } setCheckIgredient(obj);
-  }, [ingredients.length]);
+    if (checkIgredient === {}) {
+      for (let i = 0; i < ingredients.length; i += 1) {
+        obj[i] = false;
+      } setCheckIgredient(obj);
+    } else setCheckIgredient(checkIgredient);
+  }, [ingredients.length, checkIgredient]);
 
   useEffect(() => {
     const doneIngredients = Object.values(checkIgredient)
@@ -28,9 +42,122 @@ export default function DrinkInProgressCard({ fetchReturn/* , typeOfRecipe */ })
     } else setIsDisabled(true);
   }, [checkIgredient, ingredients.length]);
 
-  function handleToggle(index) {
-    return setCheckIgredient({ ...checkIgredient, [index]: !checkIgredient[index] });
+  function handleToggle(index/* , id */) {
+    return setCheckIgredient({
+      ...checkIgredient,
+      [index]: !checkIgredient[index],
+    });
   }
+
+  /* useEffect(() => {
+    const onlyTrue = Object.entries(checkIgredient).filter((el) => el[1] === true);
+    const array = onlyTrue.map((el) => ingredients[el[0]][1]);
+    return console.log(array);
+  }, [checkIgredient, ingredients]); */
+
+  /* useEffect(() => {
+    const { idDrink } = fetchReturn;
+    const onlyTrue = Object.entries(checkIgredient).filter((el) => el[1] === true);
+    const array = onlyTrue.map((el) => ingredients[el[0]][1]);
+    const getLocalStorage = JSON.parse(localStorage.getItem('inProgressRecipes'));
+    const obj = {
+      drinks: {
+        [idDrink]: [],
+      },
+      meals: {},
+    };
+
+    if (getLocalStorage === null) {
+      return localStorage.setItem('inProgressRecipes', JSON.stringify(obj));
+    }
+    const getLocalStorage2 = JSON.parse(localStorage.getItem('inProgressRecipes'));
+    const obj2 = {
+      drinks: {
+        ...getLocalStorage2.drinks,
+        [idDrink]: array,
+      },
+      meals: {},
+    };
+    return localStorage.setItem('inProgressRecipes', JSON.stringify(obj2));
+  }, [fetchReturn, checkIgredient, ingredients]); */
+
+  function doneRecipe(obj) {
+    const object = [{
+      id: obj.idDrink,
+      type: typeOfRecipe,
+      nationality: '',
+      category: obj.strCategory,
+      alcoholicOrNot: obj.strAlcoholic,
+      name: obj.strDrink,
+      image: obj.strDrinkThumb,
+      doneDate: new Date(),
+      tags: [],
+    }];
+
+    const newObj = {
+      id: obj.idDrink,
+      type: typeOfRecipe,
+      nationality: '',
+      category: obj.strCategory,
+      alcoholicOrNot: obj.strAlcoholic,
+      name: obj.strDrink,
+      image: obj.strDrinkThumb,
+      doneDate: new Date(),
+      tags: [],
+    };
+
+    const getLocalStorage = JSON.parse(localStorage.getItem('doneRecipes'));
+
+    if (getLocalStorage === null) {
+      localStorage.setItem('doneRecipes', JSON.stringify(object));
+    } else {
+      const newLocalStorage = getLocalStorage.push(newObj);
+      localStorage.setItem('doneRecipes', JSON.stringify(getLocalStorage));
+      console.log(newLocalStorage);
+    }
+  }
+
+  function copyAndAlert(id) {
+    copy(`http://localhost:3000/${typeOfRecipe}s/${id}`);
+    setCopied(true);
+    console.log(id);
+    const oneAndAHalf = 1500;
+    setTimeout(() => {
+      setCopied(false);
+    }, oneAndAHalf);
+  }
+
+  useEffect(() => {
+    const check = favoriteLS && favoriteLS
+      .some((item) => item.id === fetchReturn.idDrink);
+    setFavorited(check);
+  }, [favoriteLS, fetchReturn]);
+
+  function addFavoritedToLS() {
+    if (favorited === false) {
+      const array = [...favoriteLS,
+        {
+          id: fetchReturn.idDrink,
+          type: typeOfRecipe,
+          nationality: '',
+          category: fetchReturn.strCategory,
+          alcoholicOrNot: fetchReturn.strAlcoholic,
+          name: fetchReturn.strDrink,
+          image: fetchReturn.strDrinkThumb,
+        }];
+      localStorage.setItem('favoriteRecipes', JSON.stringify(array));
+    } else {
+      const filtering = favoriteLS
+        .filter((element) => element.id !== fetchReturn.idDrink);
+      localStorage.setItem('favoriteRecipes', JSON.stringify(filtering));
+    }
+  }
+
+  function favoriteButton() {
+    setFavorited(!favorited);
+    addFavoritedToLS();
+  }
+
   return (
     <div>
       <img
@@ -39,17 +166,32 @@ export default function DrinkInProgressCard({ fetchReturn/* , typeOfRecipe */ })
         alt={ `Imagem da bebida ${fetchReturn.strDrink}` }
       />
       <h3 data-testid="recipe-title">{ fetchReturn.strDrink}</h3>
-      <button
-        data-testid="share-btn"
-      >
-        <img src={ shareIcon } alt="Botão de compartilhar" />
-      </button>
 
-      <button
-        data-testid="favorite-btn"
-      >
-        <img src={ blackHeartIcon } alt="Botão de favoritar" />
-      </button>
+      {copied ? <span>Link copied!</span> : (
+        <button
+          data-testid="share-btn"
+          onClick={ () => copyAndAlert(fetchReturn.idDrink) }
+        >
+          <img src={ shareIcon } alt="Botão de compartilhar" />
+        </button>)}
+
+      {favorited ? (
+        <button
+          data-testid="favorite-btn"
+          onClick={ favoriteButton }
+          src={ blackHeartIcon }
+        >
+          <img src={ blackHeartIcon } alt="Botão de favoritar" />
+        </button>
+      ) : (
+        <button
+          onClick={ favoriteButton }
+          data-testid="favorite-btn"
+          src={ whiteHeartIcon }
+        >
+          <img src={ whiteHeartIcon } alt="Botão de favoritar" />
+        </button>
+      )}
       <p data-testid="recipe-category">
         {`${fetchReturn.strAlcoholic} Drink`}
 
@@ -82,7 +224,17 @@ export default function DrinkInProgressCard({ fetchReturn/* , typeOfRecipe */ })
         { fetchReturn.strInstructions}
       </p>
 
-      <button disabled={ isDisabled } data-testid="finish-recipe-btn">Finalizar</button>
+      <Link to="/done-recipes">
+        <button
+          onClick={ () => doneRecipe(fetchReturn) }
+          disabled={ isDisabled }
+          data-testid="finish-recipe-btn"
+        >
+          Finalizar
+
+        </button>
+
+      </Link>
     </div>
   );
 }
@@ -92,6 +244,6 @@ DrinkInProgressCard.propTypes = {
   strAlcoholic: PropTypes.string.isRequired,
   strInstructions: PropTypes.string.isRequired,
   strDrink: PropTypes.string.isRequired,
-  strDrinkThumb: PropTypes.string.isRequired, /*
-  typeOfRecipe: PropTypes.string.isRequired, */
+  strDrinkThumb: PropTypes.string.isRequired,
+  typeOfRecipe: PropTypes.string.isRequired,
 };
